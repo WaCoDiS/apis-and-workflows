cba2ccc815410c5ee45b77e65f212afb68d40c7d
SebaDro <sebastian.drost@hs-bochum.de>
2018-09-11 12:10:44 +0200

    Adjust one-command script due to changed file names

--- openapi/generate-data-access.sh
+++ openapi/generate-data-access.sh
@@ -5,8 +5,8 @@ lib/openapi-generator-cli.sh generate -g spring \
     --model-package=de.wacodis.dataaccess.model \
     --api-package=de.wacodis.dataaccess.controller \
     -DdateLibrary=joda \
-    -i data-access.yml \
-    -o ./build/data-access
+    -i data-access-api.yml \
+    -o ./build/data-access-api
 
 lib/google-format-java-cli.sh --aosp --replace ./build/data-access/src/main/java/de/wacodis/dataaccess/model/* ./build/data-access/src/main/java/de/wacodis/dataaccess/controller/*
 
